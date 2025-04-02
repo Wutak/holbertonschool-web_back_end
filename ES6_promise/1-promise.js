33497f71f9c24cce2cdb689867ddeb2b664550b7
julien <julienvillessot@gmail.com>
2025-04-02 15:51:53 +0200

    2

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,11 +1,10 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    const success = true;
     if (success) {
       resolve({ status: 200, body: 'Success'});
     }
     else {
-      resolve({ new Error: The fake API is noot working currently});
+      reject({ new Error(The fake API is noot working currently));
     }
   });
 }
---
+++ ES6_promise/2-then.js
@@ -0,0 +1,5 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() (status: 200, body: 'success'))
+    .catch(() new Error())
+    .finally(() ('Got a response from the API')
