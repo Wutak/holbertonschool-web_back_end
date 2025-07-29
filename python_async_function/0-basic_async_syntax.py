4bfb1a759dce09f8fab52f6ea4de460a4e55f67d
julien <julienvillessot@gmail.com>
2025-07-29 15:53:32 +0200

    push 0-4

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""asynchronous"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """function async"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""runtime"""
+
+import time
+import asyncio
+from 1-wait_n import wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """runtime"""
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    total_time = end - start
+    return total_time / n
