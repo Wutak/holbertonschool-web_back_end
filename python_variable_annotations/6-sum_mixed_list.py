ca645ae9e3eb866ca0b7b49460105753a5ae692f
julien <julienvillessot@gmail.com>
2025-08-01 13:03:24 +0200

    push

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """sum mixed list"""
-from typing import List
+from typing import List, Union
+"""sum mixed list"""
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]) -> float:
     """sum mixed list"""
     return sum(mxd_lst)
